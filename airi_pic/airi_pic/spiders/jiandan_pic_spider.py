6eda14432cc455383288ff2b929d66aaae42ac3f
BigRong <2912755@qq.com>
2020-11-25 20:45:29 +0800

    workable scrapy for picture
    it works

--- airi_pic/airi_pic/spiders/jiandan_pic_spider.py
+++ airi_pic/airi_pic/spiders/jiandan_pic_spider.py
@@ -11,22 +11,26 @@ import base64
 Page = 0
 class JiandanPicSpider(scrapy.Spider):
     name = 'jiandan_pic'
-    start_urls = ['http://jandan.net/ooxx/page-1#comments']
+    start_urls = ['http://jandan.net/ooxx/']
 
     def parse(self, response):
         global Page
         sel = scrapy.Selector(response)
 
         image_urls = sel.xpath(
-            '//ol[@class="commentlist"]//div[@class="row"]/div[@class="text"]//span[@class="img-hash"]/text()').extract()
+            '//ol[@class="commentlist"]//div[@class="row"]//div[@class="text"]//img/@src').extract()
 
         image_names = sel.xpath(
-            '//ol[@class="commentlist"]//div[@class="row"]/div[@class="text"]//span[@class="righttext"]/a/text()').extract()
+            '//ol[@class="commentlist"]//div[@class="row"]//div[@class="text"]//span[@class="righttext"]/a/text()').extract()
+
 
         new_urls= []
         for xxx in image_urls:
-            url = base64.b64decode(xx).decode('utf-8')
-            new_urls.append('https' + url)
+            # url = base64.b64decode(xxx).decode('utf-8')
+            # new_urls.append('https' + url)
+            # print("Debug #############################")
+            # print(xxx)
+            new_urls.append('https:' + xxx)
 
         item = JiandanPicItem()
         item['image_url'] = new_urls
@@ -34,9 +38,13 @@ class JiandanPicSpider(scrapy.Spider):
 
         yield item
 
-        while Page <2:
-            next_page = response.xpath('//div[@class="cp-pagenavi"/a[@class="next-comment-page"]/@href')
+        if Page <200:
+            # print("Before Debug!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            next_page = response.xpath('//div[@class="comments"]//div[@class="cp-pagenavi"]//a[@class="previous-comment-page"]/@href')
+            # print("Debug!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             if next_page:
-                url = response.urljoin(next_page[0].extract())
+                # print("next_page is %s"%next_page)
+                url = response.urljoin(next_page[1].extract())
+                print("Debug%s"%url)
                 yield scrapy.Request(url, self.parse)
             Page += 1
---
+++ jandanSpider.py
@@ -0,0 +1,46 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/11/24 18:31
+# @Author : BigRong
+# @File : jandanSpider.py
+# @Software : PyCharm
+import urllib
+
+import requests
+from lxml import etree
+
+
+def main():
+    #url
+    url = 'http://jandan.net/ooxx'
+    kv = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
+
+    i = 0;
+    j = 0;
+    while i < 20:
+        html = ''
+        r = requests.get(url, headers=kv)
+        html = r.text
+        # print(html)
+        i += 1
+        e = etree.HTML(html)
+        image_urls = e.xpath(
+            '//ol[@class="commentlist"]//div[@class="row"]//div[@class="text"]//img/@src')
+
+        image_names = e.xpath(
+            '//ol[@class="commentlist"]//div[@class="row"]//div[@class="text"]//span[@class="righttext"]/a/text()')
+
+
+        for url in image_urls:
+            urllib.request.urlretrieve("http:" + url,
+                                       ".\pic\jiandan\{}.jpg".format(j))
+            j += 1
+        url = "http:" + e.xpath(
+            '//div[@class="comments"]//div[@class="cp-pagenavi"]//a[@class="previous-comment-page"]/@href')[0]
+        print(image_urls)
+        print(image_names)
+        print(url)
+
+if __name__ == "__main__":
+    main()
+
